fcacf3c6310bcfa13f4c5ba95fde196086ea0dbc
Grégory <claudejurado@orange.fr>
2017-03-27 10:29:26 +0200

    affichage dans la console de la valeur de l'index5

--- array/app.js
+++ array/app.js
@@ -1,3 +1,4 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","dececmbre"];
  console.log(month[2]);
+ console.log(month[5]);
