759e9585ce58263d935f233d6d1df73c322c845a
Grégory <claudejurado@orange.fr>
2017-03-27 10:27:37 +0200

    affichage dans la console du mois de mars(troisième ligne du tableau"

--- array/app.js
+++ array/app.js
@@ -1,3 +1,3 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var month = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","dececmbre"];
-console.log ("hello world");
+ console.log(month[2]);
