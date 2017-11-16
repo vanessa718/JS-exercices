36bdf5defcfc875b086c909c8b480cb3ec00532e
vanessa <vanessa.chaillou@orange.fr>
2017-11-16 20:38:54 +0100

    jusqu'à ex8

--- array/app.js
+++ array/app.js
@@ -1,20 +1,27 @@
-// var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 //exercice 1
 var mois =['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre'];
 console.log(mois);
 
 //excercice 2
-for (var i =0; i <= 11; i++) {
-if (i===2) {
-console.log(mois[i]);
-}
-}
+console.log(mois[2]);
 
 //exercice 3
 console.log(mois[5]);
 
 //exercice 4
-console.log('Novembre')
+console.log(mois[10]);
 
 //exercice 5
+mois [7] =['Août'];
+console.log(mois[7]);
+
+//exercice 6
+for (var i = 0; i < mois.length; i++) {
+console.log(mois[i]);
+}
+
+//exercice 8
+array.push('courgette');
+console.log(array);
